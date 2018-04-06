c470b9a68aea3350c82245615ad35da4b819d903
thyal <thomasdfrivold@gmail.com>
2018-04-06 12:14:33 +0200

    basic button setup - onClickListeneres

--- app/src/main/java/no/thomasfrivold/tictactoe/view/GameActivity.java
+++ app/src/main/java/no/thomasfrivold/tictactoe/view/GameActivity.java
@@ -2,11 +2,15 @@ package no.thomasfrivold.tictactoe.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import no.thomasfrivold.tictactoe.R;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "GameActivity";
 
     Button btnSinglePlayer;
     Button btnTwoPlayer;
@@ -15,5 +19,24 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        btnSinglePlayer = findViewById(R.id.btn_singleplayer);
+        btnTwoPlayer = findViewById(R.id.btn_twoplayer);
+        btnSinglePlayer.setOnClickListener(this);
+        btnTwoPlayer.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        //Find out what was clicked
+        int viewId = v.getId();
+
+        if(viewId == R.id.btn_singleplayer) {
+            Log.d(TAG, "onClick: singleplayer");
+        }
+        if(viewId == R.id.btn_twoplayer) {
+            Log.d(TAG, "onClick: twoplayer");
+        }
     }
 }
