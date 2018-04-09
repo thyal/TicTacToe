f754ef3d22e6d3287a1182ca5c225e5392897d4a
thyal <thomasdfrivold@gmail.com>
2018-04-09 13:13:19 +0200

    added dialogFragment

--- app/src/main/java/no/thomasfrivold/tictactoe/view/MenuScreenFragment.java
+++ app/src/main/java/no/thomasfrivold/tictactoe/view/MenuScreenFragment.java
@@ -3,7 +3,6 @@ package no.thomasfrivold.tictactoe.view;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
---
+++ app/src/main/java/no/thomasfrivold/tictactoe/view/NameInputDialogFragment.java
@@ -0,0 +1,40 @@
+package no.thomasfrivold.tictactoe.view;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import no.thomasfrivold.tictactoe.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NameInputDialogFragment extends DialogFragment {
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("TEST")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
