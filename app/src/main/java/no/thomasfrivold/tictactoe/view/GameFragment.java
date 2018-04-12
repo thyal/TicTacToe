4337a2bd401c8683d616b9dd10b0ff33884629f0
thyal <thomasdfrivold@gmail.com>
2018-04-12 14:07:36 +0200

    playerOne name will now show in gameFragment

--- app/src/main/java/no/thomasfrivold/tictactoe/view/GameFragment.java
+++ app/src/main/java/no/thomasfrivold/tictactoe/view/GameFragment.java
@@ -2,11 +2,16 @@ package no.thomasfrivold.tictactoe.view;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import no.thomasfrivold.tictactoe.R;
 
@@ -15,6 +20,8 @@ import no.thomasfrivold.tictactoe.R;
  */
 public class GameFragment extends Fragment {
 
+    private String playerOne;
+    private String playerTwo;
 
     public GameFragment() {
         // Required empty public constructor
@@ -24,12 +31,18 @@ public class GameFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        String playerOne = this.getArguments().getString("playerOne");
-        String playerTwo = this.getArguments().getString("playerTwo");
+        playerOne = this.getArguments().getString("playerOne");
+        playerTwo = this.getArguments().getString("playerTwo");
 
-        Log.d("GAMEFRAGMENT", playerOne + " " + playerTwo);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_game, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView txt = getView().findViewById(R.id.txtv_playerOne);
+        txt.setText(playerOne);
+    }
 }
