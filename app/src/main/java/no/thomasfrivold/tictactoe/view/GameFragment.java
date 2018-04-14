fe0341ea1dd8e718c0f097683a8655ed495ae0c3
thyal <thomasdfrivold@gmail.com>
2018-04-14 18:38:48 +0200

    setup basic game logic

--- app/src/main/java/no/thomasfrivold/tictactoe/view/GameFragment.java
+++ app/src/main/java/no/thomasfrivold/tictactoe/view/GameFragment.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -18,10 +19,15 @@ import no.thomasfrivold.tictactoe.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GameFragment extends Fragment {
+public class GameFragment extends Fragment implements View.OnClickListener {
 
     private String playerOne;
     private String playerTwo;
+
+    private ImageButton img_btn_01,img_btn_02,img_btn_03,
+                        img_btn_11,img_btn_12,img_btn_13,
+                        img_btn_21,img_btn_22,img_btn_23;
+    private ImageButton[] img_btns;
 
     public GameFragment() {
         // Required empty public constructor
@@ -36,7 +42,9 @@ public class GameFragment extends Fragment {
 
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false);
+        View v =  inflater.inflate(R.layout.fragment_game, container, false);
+        initWidgets(v);
+        return v;
     }
 
     //getView() is only available after onCreateView() method has run.
@@ -48,5 +56,69 @@ public class GameFragment extends Fragment {
 
         txtPlayerOne.setText(playerOne);
         txtPlayerTwo.setText(playerTwo);
+    }
+
+    private void initWidgets(View v) {
+        img_btn_01 = v.findViewById(R.id.img_btn_01);
+        img_btn_02 = v.findViewById(R.id.img_btn_02);
+        img_btn_03 = v.findViewById(R.id.img_btn_03);
+        img_btn_11 = v.findViewById(R.id.img_btn_11);
+        img_btn_12 = v.findViewById(R.id.img_btn_12);
+        img_btn_13 = v.findViewById(R.id.img_btn_13);
+        img_btn_21 = v.findViewById(R.id.img_btn_21);
+        img_btn_22 = v.findViewById(R.id.img_btn_22);
+        img_btn_23 = v.findViewById(R.id.img_btn_23);
+        img_btns = new ImageButton[]
+                {img_btn_01, img_btn_02, img_btn_03, img_btn_11,img_btn_12,
+                img_btn_13,img_btn_21,img_btn_22,img_btn_23};
+
+        for(ImageButton img_btn : img_btns) {
+            img_btn.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+
+        switch (viewId) {
+            case R.id.img_btn_01:
+                makeMove(img_btn_01);
+                break;
+            case R.id.img_btn_02:
+                makeMove(img_btn_02);
+                break;
+            case R.id.img_btn_03:
+                makeMove(img_btn_03);
+                break;
+            case R.id.img_btn_11:
+                makeMove(img_btn_11);
+                break;
+            case R.id.img_btn_12:
+                makeMove(img_btn_12);
+                break;
+            case R.id.img_btn_13:
+                makeMove(img_btn_13);
+                break;
+            case R.id.img_btn_21:
+                makeMove(img_btn_21);
+                break;
+            case R.id.img_btn_22:
+                makeMove(img_btn_22);
+                break;
+            case R.id.img_btn_23:
+                makeMove(img_btn_23);
+                break;
+        }
+    }
+
+    void makeMove(ImageButton btn) {
+        //TODO check which player is playing
+        int image = R.drawable.cross;
+        for(ImageButton img_btn : img_btns) {
+            if(btn == img_btn) {
+                img_btn.setImageResource(image);
+            }
+        }
     }
 }
