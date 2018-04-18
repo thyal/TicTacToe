8dc1a3bde962f6082108b396f6ac3e26d2b09f2c
thyal <thomasdfrivold@gmail.com>
2018-04-18 20:06:22 +0200

    basic setup gamecontroller++

---
+++ app/src/main/java/no/thomasfrivold/tictactoe/data/Player.java
@@ -0,0 +1,28 @@
+package no.thomasfrivold.tictactoe.data;
+
+public class Player {
+
+    private String name;
+    private int wins;
+
+    public Player(String name, int wins) {
+        this.name = name;
+        this.wins = wins;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+}
--- app/src/main/java/no/thomasfrivold/tictactoe/logic/GameController.java
+++ app/src/main/java/no/thomasfrivold/tictactoe/logic/GameController.java
@@ -1,26 +1,21 @@
 package no.thomasfrivold.tictactoe.logic;
 
-import java.util.Arrays;
-
 public class GameController {
 
-    int winnerCombinations[][] = {{0,1,2},{0,3,6},{0,4,8},{3,4,5},{1,4,7},{2,5,8},{2,4,6},{3,4,5}};
+    int[][] board;
+    int movecounter;
 
     public GameController() {
+        this.board = new int[3][3];
     }
 
-    public boolean checkWin(int[] row) {
-        for(int winnerCombination[] : winnerCombinations) {
-            Arrays.sort(winnerCombination);
-            Arrays.sort(row);
-            System.out.println(row);
-            if(Arrays.equals(winnerCombination, row)) {
-                System.out.println("WINNER");
-                return true;
-            } else {
-                System.out.println("NOT WIN");
-            }
-        }
-        return false;
+    public GameController(int cols, int rows) {
+        this.board = new int[cols][rows];
     }
+
+    public void makeMove(int col, int row) {
+
+    }
+
+
 }
